dbf3ff52b829009b6185a0b8b586d0d9272a489b
BurritoBlankets <jgdl2003@gmail.com>
2023-03-13 15:16:29 -0700

    File Createdgit status

---
+++ Bootleg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+###############################################################################
+#File:      Bootleg.sh
+#Author:    Jose (Pillo) Duenas-Lopez & Jane Mcclure
+#Info:
+#           CI201 group project: A simple bash script that performs ping sweep
+#           and port scans.
+#
+#Class:      CI 201 @ 1200-1250 (M,W,F)
+###############################################################################
+
+
+clear
